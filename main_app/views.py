9fb16631500d8192f00ea64a53a106905d4f9eef
Sky <anvandp18@gmail.com>
2018-09-28 11:50:06 -0700

    now print all the data from database in Postico

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
 # Create your views here.
 
-class Cat:
-	def __init__(self, name, breed, description, age):
-		self.name = name
-		self.breed = breed
-		self.description = description
-		self.age = age
 
-cats = [
-	Cat('Lolo', 'tabby', 'foul little demon', 3),
-	Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0 ),
-	Cat('Raven', 'black tripod', '3-legged feline', 4)
-]
 def index(request):
+	cats = Cat.objects.all()
 	return render(request, 'index.html', {'cats': cats})
