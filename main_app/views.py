e6a11bba809a6931d4c772de1f023037d821fdac
Sky <anvandp18@gmail.com>
2018-09-28 13:25:13 -0700

    create post function to create a cat

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cat
+from .forms import CatForm
+from django.http import HttpResponse, HttpResponseRedirect
+
 # Create your views here.
 
 
@@ -11,3 +14,14 @@ def index(request):
 def show(request, cat_id):
 	cat = Cat.objects.get(id=cat_id)
 	return render(request, 'show.html', {'cat': cat})
+
+def post_cat(request):
+		form = CatForm(request.POST)
+		if form.is_valid():
+        	cat = Cat(
+        	    name=form.cleaned_data['name'],
+         	   breed=form.cleaned_data['breed'],
+         	   description=form.cleaned_data['description'],
+         	   age=form.cleaned_data['age'])
+       		cat.save()
+    return HttpResponseRedirect('/')
