1539c1b9aa5a1b81c3f3ec33e65c7a7e77d59077
Sky <anvandp18@gmail.com>
2018-09-28 11:18:22 -0700

    create data for cat and refresh the localhost to check

--- main_app/views.py
+++ main_app/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Cat:
+	def __init__(self, name, breed, description, age):
+		self.name = name
+		self.breed = breed
+		self.description = description
+		self.age = age
+
+cats = [
+	Cat('Lolo', 'tabby', 'foul little demon', 3),
+	Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 4 ),
+	Cat('Raven', 'black tripod', '3-legged feline', 4)
+]
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'index.html', {'cats': cats})
