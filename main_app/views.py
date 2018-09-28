3e7c2a17fe0987bc7a6011db33ffd792cec205cc
Sky <anvandp18@gmail.com>
2018-09-28 15:21:54 -0700

    a user could add a cat now but the view is ugly

--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,6 @@ from .models import Cat
 from .forms import CatForm
 from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
-
-
 def index(request):
 	cats = Cat.objects.all()
 	form = CatForm()
@@ -17,12 +14,12 @@ def show(request, cat_id):
 	return render(request, 'show.html', {'cat': cat})
 
 def post_cat(request):
-		form = CatForm(request.POST)
-		if form.is_valid():
-        	cat = Cat(
-        	    name=form.cleaned_data['name'],
-         	   breed=form.cleaned_data['breed'],
-         	   description=form.cleaned_data['description'],
-         	   age=form.cleaned_data['age'])
-       		cat.save()
-    return HttpResponseRedirect('/')
+	form = CatForm(request.POST)
+	if form.is_valid():
+		cat = Cat(
+			name=form.cleaned_data['name'],
+			breed=form.cleaned_data['breed'],
+			description=form.cleaned_data['description'],
+			age=form.cleaned_data['age'])
+		cat.save()
+	return HttpResponseRedirect('/')
