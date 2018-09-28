b5e855b46b81d026790004b5bcbfd5f9539507b1
Sky <anvandp18@gmail.com>
2018-09-28 13:23:00 -0700

    set up the route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^', views.index, name='index'),
-	url(r'^<int:cat_id>/$', views.show, name='show')
+	url(r'^<int:cat_id>/$', views.show, name='show'),
+	url(r'post_url/$', views.post_cat, name='post_cat')
 ]
